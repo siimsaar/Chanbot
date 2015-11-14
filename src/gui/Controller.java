33eeba442967d36fee08f0bdeedc81875d2a193e
sisaar <siim.saar@itcollege.ee>
2015-11-14 23:13:01 +0200

    GUI elements are now functional

--- src/gui/Controller.java
+++ src/gui/Controller.java
@@ -2,16 +2,20 @@ package gui;
 
 import core.Topic;
 import javafx.application.Platform;
+import javafx.beans.value.ObservableListValue;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -22,24 +26,37 @@ public class Controller extends OutputStream implements Initializable {
     @FXML
     public Button startButton;
     @FXML
-    public ToggleButton enableWEBM;
+    public CheckBox enableWEBM;
     @FXML
     public TextArea stdout;
+    @FXML
+    public ComboBox<String> topic = new ComboBox<>();
+    @FXML
+    public TextField refreshInt;
+
 
     public void startThread(ActionEvent event) {
-        Topic kek = new Topic("mu", "/home/ayy/dlthing/", "/home/ayy/dlthing/catalogMu.json", "mu");
-        if(dlWEBM){
-            kek.setWEBM();
+        System.out.println(refreshInt.getText());
+        try {
+            if (topic.getValue().equals("/mu/ - kpg")) {
+                Topic kek = new Topic("mu", "/home/ayy/dlthing/", "/home/ayy/dlthing/catalogMu.json", "mu");
+                if (enableWEBM.isSelected()) {
+                    kek.setWEBM();
+                }
+                kek.start();
+            } else if (topic.getValue().equals("/p/ - Recent Photo")) {
+                Topic kuk = new Topic("p", "/home/ayy/dlthing/", "/home/ayy/dlthing/catalogP.json", "p");
+                kuk.start();
+            }
+        } catch (NullPointerException e) {
+            System.out.println("[WARNING] Pick a topic!");
         }
-        kek.start();
-    }
-    public void enableWEBM(ActionEvent event) {
-        boolean dlWEBM = true;
     }
 
     //this is from stackoverflow, i have no idea how it works
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        topic.getItems().addAll("/mu/ - kpg", "/p/ - Recent Photo");
         OutputStream out = new OutputStream() {
             @Override
             public void write(int b) throws IOException {
--- src/threads/ThreadManager.java
+++ src/threads/ThreadManager.java
@@ -5,13 +5,12 @@ import gui.Gui;
 import hashing.Hasher;
 
 public class ThreadManager {
+
     public static void main(String[] args) {
         Gui.initGui(args);
-        //Topic kek = new Topic("mu", "/home/ayy/dlthing/", "/home/ayy/dlthing/catalogMu.json", "mu");
-        //kek.start();
-        //Topic kuk = new Topic("p", "/home/ayy/dlthing/", "/home/ayy/dlthing/catalogP.json", "p");
-        //kuk.start();
-        //Topic kik = new Topic("wg", "/home/ayy/dlthing/", "/home/ayy/dlthing/catalogwg.json", "wg");
-        //kik.start();
+        //TODO
+    }
+    public static void threadFactory() {
+    //TODO
     }
 }
