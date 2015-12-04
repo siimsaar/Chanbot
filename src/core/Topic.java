734d8831cb4319e3dafdea1d17f55875929bff19
siimsaar <ziim123@live.com>
2015-12-04 09:52:51 +0200

    Update Topic.java

--- src/core/Topic.java
+++ src/core/Topic.java
@@ -44,13 +44,13 @@ public class Topic extends ImgBoard {
     String locate4ch() {
         String dataString;
         jsonCatalog = 1;
-        while (jsonCatalog <= conf.getMaxJsonPages()) {
+        while (jsonCatalog <= conf.getMaxJsonPages()) { // töötab niikaua kuni on seadetes max lehtede arv ettenähtud
             try {
-                UserAgent jsonReader = new UserAgent();
+                UserAgent jsonReader = new UserAgent(); // pseudobrowser JSONi jaoks
                 jsonReader.settings.maxBytes = -1;
                 jsonReader.settings.readTimeout = 600000;
-                jsonReader.sendGET(conf.getApiUrl() + jsonCatalog + ".json");
-                JNode thread = jsonReader.json.findEvery(conf.getPattern()).findEvery("no").get(0);
+                jsonReader.sendGET(conf.getApiUrl() + jsonCatalog + ".json"); // GET päring JSON failile
+                JNode thread = jsonReader.json.findEvery(conf.getPattern()).findEvery("no").get(0); // kasutab regexit JSON failis
                 dataString = thread.toString();
                 if(!dataString.isEmpty()) {
                     return dataString;
@@ -74,15 +74,15 @@ public class Topic extends ImgBoard {
     @Override
     String locate2ch() {
         tryCount = 0;
-        while (tryCount < conf.getMaxPages()) {
+        while (tryCount < conf.getMaxPages()) { // töötab niikaua kuni on seadetes max lehtede arv ettenähtud
             try {
-                Document htmlFile = Jsoup.connect(conf.getUrl() + page()).maxBodySize(0).timeout(500000).get();
-                Element htmlLinks = htmlFile.select(conf.getPattern()).not(conf.getNotPattern()).first();
-                String threadID = htmlLinks.toString().substring(0, conf.getTopicLength()).replaceAll("[^0-9]", "");
+                Document htmlFile = Jsoup.connect(conf.getUrl() + page()).maxBodySize(0).timeout(500000).get(); // tõmbab HTML faili
+                Element htmlLinks = htmlFile.select(conf.getPattern()).not(conf.getNotPattern()).first(); // kasutab regexit HTML faili peal
+                String threadID = htmlLinks.toString().substring(0, conf.getTopicLength()).replaceAll("[^0-9]", ""); // ebavajalik välja
                 if(!threadID.isEmpty()) {
                     return (conf.getUrl() + "/res/" + threadID + ".html");
                 }
-                tryCount++;
+                tryCount++; 
             } catch (NullPointerException | IOException er) {
                 tryCount++;
             }
@@ -109,11 +109,11 @@ public class Topic extends ImgBoard {
      */
     String webmCheck() {;
         if (dlWEBM.equals("default")) {
-            return "a[href~=(?i)\\.(png|jpe?g)]:not(a[class])";
+            return "a[href~=(?i)\\.(png|jpe?g)]:not(a[class])"; // leia JPG ja PNG lingid
         } else if (dlWEBM.equals("pluswebm")) {
-            return "a[href~=(?i)\\.(png|jpe?g|webm)]:not(a[class])";
+            return "a[href~=(?i)\\.(png|jpe?g|webm)]:not(a[class])"; // leia PNG, JPG ja WEBM lingid
         } else if (dlWEBM.equals("webm")) {
-            return "a[href~=(?i)\\.(webm)]:not(a[class])";
+            return "a[href~=(?i)\\.(webm)]:not(a[class])"; // leia ainult WEBM lingid
         }
         return null;
     }
@@ -124,7 +124,7 @@ public class Topic extends ImgBoard {
      */
     @Override
     public void run() {
-        if(streaming) {
+        if(streaming) { // kui streamimine on soovitud siis käivitada thread streamina
         stream();
             Thread.currentThread().interrupt();
         }
